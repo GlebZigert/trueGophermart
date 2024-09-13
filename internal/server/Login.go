cca96ccd76516c76a21fdbdf64d3d469cf997e06
GlebZigert <GlebZigert>
2024-09-13 10:51:05 +0300

    Шлю запрос на авторизацию - с неверным паролем-логином. Должен прийти ответ со статусом 401

--- internal/server/Login.go
+++ internal/server/Login.go
@@ -5,9 +5,14 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/GlebZigert/trueGophermart/internal/auth"
+	"github.com/GlebZigert/trueGophermart/internal/logger"
 	"github.com/GlebZigert/trueGophermart/internal/packerr"
 	"github.com/GlebZigert/trueGophermart/internal/users"
+	"go.uber.org/zap"
 )
+
+var WrongPassword *users.UsersErr = &users.UsersErr{"Неверный пароль"}
 
 func Login(w http.ResponseWriter, req *http.Request) {
 
@@ -29,5 +34,37 @@ func Login(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte{})
 		return // err
 	}
+
+	//если пришла правильно составленная посылка
+	logger.Log.Info("try to logib: ", zap.String("login", user.Login), zap.String("password", user.Password))
+	//проверяем есть ли уже такой логин
+
+	finded, ok := users.Find(user.Login)
+	if ok != nil {
+
+		//если не нашлось пользователя с таким логином
+		err = users.FoundNoUser
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte{})
+		return
+	}
+
+	//если пользователь с таким логином найден - проверяем пароль
+
+	if finded.Password == user.Password {
+		//если пароли несовпадают
+		err = WrongPassword
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte{})
+		return
+	}
+
+	//если пароли совпадают - авторизовываем - даем ключ
+	jwt, _ := auth.BuildJWTString()
+	//добавляю ключ
+	w.Header().Add("Authorization", string(jwt))
+	//ставлю статус 200
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte{})
 
 }
