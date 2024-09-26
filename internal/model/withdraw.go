49214535465bc63a7e325d0b722eca05f99d1db1
di <nah>
2024-09-26 19:27:44 +0300

    .

--- internal/model/withdraw.go
+++ internal/model/withdraw.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Withdraw struct {
-	UID         int       `json :"-" `
-	Number      string    `json :"order" `
-	Sum         float32   `json :"sum"`
-	ProcessedAt time.Time `json :"processed_at"`
+	UID         int       `json:"-"`
+	Number      string    `json:"order" `
+	Sum         float32   `json:"sum"`
+	ProcessedAt time.Time `json:"processed_at"`
 }
