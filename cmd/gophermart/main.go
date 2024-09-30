a16dd515e45e52ac7e7ba424cbc025f693c65871
GlebZigert <GlebZigert>
2024-09-30 08:37:24 +0300

    .

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"net/http"
 
 	"github.com/GlebZigert/trueGophermart/internal/app"
 )
 
 func main() {
 	err := app.Run()
-	log.Fatalln(err.Error())
+
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("HTTP server error: %v", err)
+	}
+
 }
